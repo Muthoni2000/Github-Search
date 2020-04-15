5b2ea88c43f9f97cf03730d420cf28969b6b7360
Muthoni2000 <bmuthoni39@yahoo.com>
2020-04-15 06:44:11 +0300

    created component wonder

---
+++ src/app/wonder/wonder.component.ts
@@ -0,0 +1,16 @@
+ 
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wonder',
+  templateUrl: './wonder.component.html',
+  styleUrls: ['./wonder.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
